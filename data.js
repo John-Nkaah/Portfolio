70517c077fc53b7dd13547823408cd3bcd458c64
John-Nkaah <nkaah.john@gmail.com>
2024-10-09 18:56:49 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['Highly motivated and detail-oriented Frontend Web Developer with expertise in crafting responsive, user-friendly, and visually appealing web applications. Proficient in: Programming languages: HTML, CSS, JavaScript (ES6+); Frontend frameworks: React, Angular, Vue.js; UI libraries: Bootstrap, Material-UI; Tools: Git, Webpack, Babel']
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: ['Highly motivated and innovative Backend Web Developer with expertise in designing, developing, and deploying scalable, secure, and efficient server-side applications. Proficient in: Programming languages: Python, Node.js (JavaScript) as well as PHP; Backend frameworks: Django; Databases: MySQL; Tools: Git']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['Highly motivated and versatile Fullstack Web Developer with expertise in designing, developing, and deploying scalable, secure, and efficient web applications. Proficient in: Frontend: HTML/CSS/JavaScript (ES6+), React/Angular.js, Responsive design, Accessibility, Web performance optimization; Backend: Python/PHP, Django, MySQL']
+
+    },
+]
